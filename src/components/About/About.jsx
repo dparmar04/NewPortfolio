6fe77ad28eaba04ca9bfab3d37bff3b1f74d216c
dparmar <116805076+dparmar04@users.noreply.github.com>
2024-08-20 20:02:36 +0530

    Contact From

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,63 @@
 import {React, useEffect, useRef} from 'react'
 import './about.css';
+import gsap from 'gsap'
 import { useInView } from 'react-intersection-observer';
 
 const About = () => {
   const { ref, inView } = useInView({
     threshold: 0.4,    // Trigger when 10% of the image is visible
   });
+  const shapeRef = useRef(null);
+ const textRef = useRef(null);
 
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const x = (e.clientX / window.innerWidth) * 100;
+      const y = (e.clientY / window.innerHeight) * 100;
+
+      gsap.to(shapeRef.current, {
+        x: (x - 50) * 0.2, // Adjust these multipliers to control the movement
+        y: (y - 50) * 0.2,
+        ease: 'power2.out',
+        duration: 0.5,
+      });
+    };
+
+    if (inView && textRef.current) {
+      // Ensure no duplication in text
+      const myText = textRef.current.textContent;
+      textRef.current.innerHTML = ''; // Clear existing content
+
+      // Split text into characters and create spans
+      const chars = Array.from(myText).map((char) => {
+        const span = document.createElement('span');
+        span.textContent = char;
+        textRef.current.appendChild(span);
+        return span;
+      });
+
+      gsap.set(textRef.current, { perspective: 400 });
+
+      gsap.from(chars, {
+        duration: 0.8,
+        opacity: 0,
+        scale: 0,
+        y: 80,
+        rotationX: 180,
+        transformOrigin: '0% 50% -50',
+        ease: 'back',
+        stagger: 0.02,
+      });
+    }
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, [inView]);
+
+   
   return (
     <div className="section about "  ref={ref}>
    <div class="marquee-container">
@@ -26,10 +77,10 @@ const About = () => {
     <div className={`about-bg ${inView ? 'reveal' : ''}`} >
         <div class="shape"></div>
       <div className='about-content'>
-        <h1  className={`typewriter-text ${inView ? 'reveal' : ''}`}>I’m <span className='special-text'>Dhruv Parmar</span>, a web wizard crafting magic with React and juggling a tech degree. When I’m not fixing bugs, I’m perfecting animations and geeking out over new tech. Want to know more? Let’s connect and build something awesome together!</h1>
+        <h1 className={`typewriter-text ${inView ? 'reveal' : ''}`} ref={textRef}><span className='special-text'>I’m Dhruv Parmar</span>, a web wizard crafting magic with React and juggling a tech degree. When I’m not fixing bugs, I’m perfecting animations and geeking out over new tech. Want to know more? Let’s connect and build something awesome together!</h1>
       </div>
 
-      <img src="https://images.unsplash.com/photo-1567282026984-fbad0228317d?w=350" alt="Background" className={`image ${inView ? 'reveal' : ''}`} />
+      <img ref={shapeRef} src="https://images.unsplash.com/photo-1567282026984-fbad0228317d?w=350" alt="Background" className={`image ${inView ? 'reveal' : ''}`} />
       
     </div>
   </div>
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './contact.css';
+import gsap from 'gsap'
 
 const Contact = () => {
+  const contactBgRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const x = (e.clientX / window.innerWidth) * 100;
+      const y = (e.clientY / window.innerHeight) * 100;
+
+      gsap.to(contactBgRef.current, {
+        backgroundPosition: `${x}% ${y}%`,
+        ease: 'power2.out',
+         duration: 1,
+        delay:0.3,
+      });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+   
   return (
-    <div className='section contact'>
+    <div className='section contact' ref={contactBgRef}>
       <h1 className='title'>Contact</h1>
         <div className='contact-container'>
-          <div className='contact-form'>
-          <form>
+           <div className='contact-form'>
+              
+              
+          <form  method="post" action='https://script.google.com/macros/s/AKfycbwkJq70X23UrLY2ln8pA16EEKN-WRDz9uQa7DPj8bfEBgrotsiOy-v3oEOgGmFmOdDeCA/exec'>
             <div className='form-group'>
               <input type="text" id="fname" name="fname" required placeholder="FIRST NAME" />
 
